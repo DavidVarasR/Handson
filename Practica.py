06c19a0ad6fb0b1b4673cf8d388b5f7a53bb4d7c
DavidVarasR <114000731+DavidVarasR@users.noreply.github.com>
2022-09-20 16:07:17 +0200

    pito

---
+++ Practica.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+st.title("My first Ml APP")
+st.markdown("Main Page")
+st.sidebar.markdown("Main page")
+
+st.write("Hello world")
+
+but1=st.button("Como me van a ir las practicas?")
+if but1:
+	from PIL import Image
+	image = Image.open('pepowhy.png') 
+	st.image(image,caption='buahsoy yo literal')
+
+else:
+	st.write("bye :wave:")
+
+but2=st.button("Pero, ¿y el bicho?")
+if but2:
+
+else
+ st.write("¿el bicho?")
